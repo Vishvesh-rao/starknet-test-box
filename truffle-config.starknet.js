5c98a42714fb4d673c3a402afabaa6f27fe00a51
Vishvesh a.k.a Str1d3r <vishveshsrao@gmail.com>
2022-07-25 20:11:37 +0530

    Add selective compilation feature

--- truffle-config.starknet.js
+++ truffle-config.starknet.js
@@ -39,6 +39,7 @@ module.exports = {
   // Configure your Cairo compilers
   compilers: {
     cairo: {
+      selecetive_compile: true,
       repository: "trufflesuite/cairo-starknet-cli",  // Docker Hub repository
       version: "0.9.0",                               // Version tag
     }
