5e58a1c38905bd8879b199bb4653673e76639823
Vishvesh a.k.a Str1d3r <vishveshsrao@gmail.com>
2022-07-27 21:47:22 +0530

    Add warp deploy

---
+++ scripts/cairo_deploy.mjs
@@ -0,0 +1,19 @@
+import { run } from './exec_shellcmd.mjs';
+import jetpack from 'fs-jetpack';
+
+let cairo_contracts = [];
+let warp = "/home/vishvesh/.config/yarn/global/node_modules/@nethermindeth/warp/bin/warp";
+
+let inputs = process.argv[2];
+
+async function deploy(){
+    const src = jetpack.cwd("/contracts/starknet/");
+    cairo_contracts = src.find({ matching: "*.cairo" });
+
+    for (let i=0; i < cairo_contracts.length; i++) {
+        stdout = await run(warp + " deploy " + "contracts/starkent/" + cairo_contracts[i] + " --inputs " + inputs);
+    }
+
+    return stdout;
+}
+
