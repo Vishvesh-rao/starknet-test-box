0253dedd94dc44669aa36167ef2e18dbe5ef7f2a
Vishvesh a.k.a Str1d3r <vishveshsrao@gmail.com>
2022-08-04 14:59:39 +0530

    Update compile_starknet.mjs

--- scripts/compile_starknet.mjs
+++ scripts/compile_starknet.mjs
@@ -3,10 +3,10 @@ import starknetConfig from '../truffle-config.starknet.js';
 import { get_contracts, get_contracts_selectively, transpile, del_dir } from './cairo_transpiler.mjs'
 
 if (starknetConfig.compilers.cairo.selecetive_compile){
-    copy_contracts_selectively();
+    get_contracts_selectively();
 }
 else{
-    copy_contracts();
+    get_contracts();
 }
 
 let {stdout} = await transpile();
