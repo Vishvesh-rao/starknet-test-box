b9c5af46012605e0701081cf2c35fdc085f695bf
VIshvesh <vishveshsrao@gmail.com>
2022-08-06 13:01:34 +0530

    Update transpilation process

--- scripts/cairo_transpiler.mjs
+++ scripts/cairo_transpiler.mjs
@@ -1,40 +1,68 @@
 import { run } from './exec_shellcmd.mjs';
 import jetpack from 'fs-jetpack';
 import rimraf from 'rimraf';
+import fs from 'fs';
 
-let sol_contracts = [];
+let l2_contracts = [];
+let l1_contracts = [];
+let warp = "/home/vishvesh/.config/yarn/global/node_modules/@nethermindeth/warp/bin/warp";
 let { stdout } = "";
+let selective_compile = false;
 
 function get_contracts(){
-    const src = jetpack.cwd("contracts/ethereum/");
+  const src = jetpack.cwd("contracts/starknet/");
+  l2_contracts = src.find({ matching: "*.sol" });
 
-    sol_contracts = src.find({ matching: "*.sol" });
-}
+  src = jetpack.cwd("contracts/ethereum/");
+  l1_contracts = src.find({ matching: "cairo_*.sol" });
 
-function get_contracts_selectively(){
-  const src = jetpack.cwd("contracts/ethereum/");
-
-  sol_contracts = src.find({ matching: "cairo_*.sol" });
+  if (l1_contracts.length > 0){
+    selective_compile = true;
+  }
 }
 
 async function transpile(){
-  for (let i=0; i < sol_contracts.length; i++) {
-    stdout = await run("warp" + " transpile " + "contracts/ethereum/" + sol_contracts[i]);
+  console.log("inside transpile")
+  for (let i=0; i < l2_contracts.length; i++) {
+    stdout = await run(warp + " transpile " + "contracts/starknet/" + l2_contracts[i]);
   }
 
+  if (selective_compile){
+    for (let i=0; i < l1_contracts.length; i++) {
+      stdout = await run(warp + " transpile " + "contracts/ethereum/" + l1_contracts[i]);
+    }
+  }
   return stdout;
 }
 
 function del_dir(){
+  const src = jetpack.cwd("warp_output/contracts/starknet/");
+  const dst = jetpack.cwd("contracts/starknet/");
+  let cairo_contracts = src.find({ matching: "*.cairo" });
+
+  cairo_contracts.forEach(filePath => {
+    src.copy(filePath, dst.path(filePath));
+  });
+
+  for (let i=0; i < l2_contracts.length; i++) {
+    fs.unlinkSync("contracts/starknet/"+l2_contracts[i]);
+  }
+
+  if (selective_compile){
     const src = jetpack.cwd("warp_output/contracts/ethereum/");
     const dst = jetpack.cwd("contracts/starknet/");
     let cairo_contracts = src.find({ matching: "*.cairo" });
-
+  
     cairo_contracts.forEach(filePath => {
       src.copy(filePath, dst.path(filePath));
     });
 
-    rimraf.sync("warp_output/");
+    for (let i=0; i < l1_contracts.length; i++) {
+      fs.unlinkSync("contracts/ethereum/"+l1_contracts[i]);
+   }
+  }
+
+  rimraf.sync("warp_output/");
 }
 
-export{get_contracts, get_contracts_selectively, transpile, del_dir}
+export{get_contracts, transpile, del_dir}
--- scripts/compile_starknet.mjs
+++ scripts/compile_starknet.mjs
@@ -1,14 +1,9 @@
 
-import starknetConfig from '../truffle-config.starknet.js';
-import { get_contracts, get_contracts_selectively, transpile, del_dir } from './cairo_transpiler.mjs'
+import { get_contracts, transpile, del_dir } from './cairo_transpiler.mjs'
 
-if (starknetConfig.compilers.cairo.selecetive_compile){
-    get_contracts_selectively();
-}
-else{
-    get_contracts();
-}
+get_contracts();
 
+console.log("l2 compilation done!!")
 let {stdout} = await transpile();
 del_dir();
 
